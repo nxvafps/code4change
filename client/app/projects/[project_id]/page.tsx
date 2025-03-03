d610a509fa09962167280820c502c365a966f592
markamcfadden <mcfadden.a.mark@gmail.com>
2025-03-03 10:11:19 +0000

    initial setup for pages and components

---
+++ client/app/projects/[project_id]/page.tsx
@@ -0,0 +1,3 @@
+export default function ProjectDetails() {
+  return <p>This is the page for an individual project</p>;
+}
---
+++ client/app/register/page.tsx
@@ -0,0 +1,3 @@
+export default function Register() {
+  return <p>This is the Register Page</p>;
+}
