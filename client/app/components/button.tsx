f7a7bad74210ad452e0e3a44c706fdff0dcd1eb9
Nicholas Canny <nicholascanny@macbook-pro-100.home>
2025-03-03 16:48:22 +0000

    add styled login and register buttons

--- client/app/components/button.tsx
+++ client/app/components/button.tsx
@@ -1,45 +1,27 @@
 import styled from "styled-components";
 
 const StyledButton = styled.button`
-background-color: #24292e;
-color: white;
-font-size: 16rem;
-border-radius: 0.375rem;
-cursor: pointer;
+  background-color: #24292e;
+  color: rgb(255, 255, 255);
+  font-size: 1rem;
+  border-radius: 0.375rem;
+  cursor: pointer;
+  width: 150px;
+  height: 30px;
   &:hover {
     background-color: #1b1f23;
   }
+`;
 
-`
+function LoginButton() {
+  const handleLogin = () => {};
 
-
- function LoginButton(){
-    const handleLogin = () => {
-       
-      };
-
-    return (
-        <StyledButton onClick={handleLogin}>Login</StyledButton>
-    )
-
-
+  return <StyledButton onClick={handleLogin}>Login</StyledButton>;
 }
 
-function RegisterButton(){
-
-    const handleRegister = () => {
-       
-    };
-    return (
-        <StyledButton onClick={handleRegister}>Register</StyledButton>
-    )
-
-    
+function RegisterButton() {
+  const handleRegister = () => {};
+  return <StyledButton onClick={handleRegister}>Register</StyledButton>;
 }
 
-
-export {LoginButton, RegisterButton}
-
-
-
-`
+export { LoginButton, RegisterButton };
