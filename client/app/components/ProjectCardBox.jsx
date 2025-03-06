9608891a834d2d17f833353f170435b182d64320
Alsamri <azozid@yahoo.com>
2025-03-06 12:35:02 +0000

    all project fetch

--- client/app/components/ProjectCardBox.jsx
+++ client/app/components/ProjectCardBox.jsx
@@ -5,43 +5,6 @@ import styled from "styled-components";
 import ProjectCard from "./ProjectCard";
 
 export default function ProjectCardBox() {
-  const [projects, setProjects] = useState([
-    {
-      project_id: 1,
-      owner_username: "nxvafps",
-      project: {
-        name: "EcoTracker",
-        description: "An app to track and reduce your carbon footprint",
-        github_repo_url: "https://github.com/nxvafps/EcoTracker",
-        project_image_url: "https://example.com/images/ecotracker.jpg",
-        status: "active",
-      },
-    },
-    {
-      project_id: 2,
-      owner_username: "projectowner",
-      project: {
-        name: "AccessibilityHelper",
-        description: "A toolkit for making websites more accessible",
-        github_repo_url: "https://github.com/projectowner/AccessibilityHelper",
-        project_image_url: "https://example.com/images/accessibility.jpg",
-        status: "active",
-      },
-    },
-    {
-      project_id: 3,
-      owner_username: "projectowner",
-      project: {
-        name: "RefugeeConnect",
-        description:
-          "Platform connecting volunteers with refugee support initiatives",
-        github_repo_url: "https://github.com/projectowner/RefugeeConnect",
-        project_image_url: "https://example.com/images/refugee-connect.jpg",
-        status: "planning",
-      },
-    },
-  ]);
-
   return (
     <Container>
       {projects.map((project, index) => (
