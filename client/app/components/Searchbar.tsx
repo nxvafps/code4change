3b7715da31453c65c95e7667bf24c729fa833642
markamcfadden <mcfadden.a.mark@gmail.com>
2025-03-10 12:12:27 +0000

    add apply filters button to searchbar

--- client/app/components/Searchbar.tsx
+++ client/app/components/Searchbar.tsx
@@ -25,6 +25,8 @@ export default function SearchBar({
 }: SearchBarProps) {
   const [categories, setCategories] = useState<Category[]>([]);
   const [skills, setSkills] = useState<Skill[]>([]);
+  const [localCategory, setLocalCategory] = useState<string>("");
+  const [localSkill, setLocalSkill] = useState<string>("");
 
   useEffect(() => {
     const fetchData = async () => {
@@ -41,10 +43,18 @@ export default function SearchBar({
     fetchData();
   }, []);
 
+  const handleSubmit = () => {
+    setSelectedCategory(localCategory);
+    setSelectedSkill(localSkill);
+  };
+
   return (
     <SearchContainer>
       <FilterLabel>Category:</FilterLabel>
-      <Select onChange={(e) => setSelectedCategory(e.target.value)}>
+      <Select
+        value={localCategory}
+        onChange={(e) => setLocalCategory(e.target.value)}
+      >
         <option value="">All Categories</option>
         {categories.map((category) => (
           <option key={category.id} value={category.category_name}>
@@ -54,7 +64,10 @@ export default function SearchBar({
       </Select>
 
       <FilterLabel>Skill:</FilterLabel>
-      <Select onChange={(e) => setSelectedSkill(e.target.value)}>
+      <Select
+        value={localSkill}
+        onChange={(e) => setLocalSkill(e.target.value)}
+      >
         <option value="">Select a skill</option>
         {skills.map((skill) => (
           <option key={skill.id} value={skill.name}>
@@ -62,6 +75,7 @@ export default function SearchBar({
           </option>
         ))}
       </Select>
+      <ApplyButton onClick={handleSubmit}>Apply Filters</ApplyButton>
     </SearchContainer>
   );
 }
@@ -69,6 +83,8 @@ export default function SearchBar({
 const SearchContainer = styled.div`
   display: flex;
   flex-direction: column;
+  justify-content: center;
+  align-items: center;
   gap: 10px;
   margin-bottom: 20px;
 `;
@@ -83,4 +99,19 @@ const Select = styled.select`
   border: 1px solid #ddd;
   background-color: white;
   cursor: pointer;
+`;
+
+const ApplyButton = styled.button`
+  background-color: ${({ theme }) => theme.colors.primary.main};
+  color: ${({ theme }) => theme.colors.text.light};
+  padding: ${({ theme }) => theme.spacing.xs} ${({ theme }) => theme.spacing.sm};
+  border-radius: ${({ theme }) => theme.borderRadius.small};
+  text-decoration: none;
+  font-size: ${({ theme }) => theme.typography.fontSize.md};
+  font-weight: ${({ theme }) => theme.typography.fontWeight.medium};
+  transition: background-color 0.2s ease;
+  &:hover {
+    background-color: ${({ theme }) => theme.colors.primary.dark};
+  }
+  width: 150px;
 `;
