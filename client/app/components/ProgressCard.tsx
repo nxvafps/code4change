c5103b5d48968cb4f18b78028e24ae6e1e26418e
Nicholas Canny <nicholascanny@macbook-pro-100.home>
2025-03-11 10:41:45 +0000

    add more progress info

--- client/app/components/ProgressCard.tsx
+++ client/app/components/ProgressCard.tsx
@@ -1,6 +1,3 @@
-// ProgressCard.tsx
-"use client";
-
 import React from "react";
 import styled from "styled-components";
 
@@ -8,22 +5,50 @@ interface ProgressCardProps {
   actualProgress: number;
 }
 
+const LEVELS = [
+  { level: "Contributor", xp: 1 },
+  { level: "Active Contributor", xp: 50 },
+  { level: "Top Contributor", xp: 250 },
+  { level: "Open Source Champion", xp: 500 },
+  { level: "Code4Change Legend", xp: 1000 },
+];
+
 const ProgressCard: React.FC<ProgressCardProps> = ({ actualProgress }) => {
+  actualProgress = 100;
+  const currentLevel = LEVELS.find((level) => actualProgress >= level.xp);
+
+  const nextLevel = LEVELS.find((level) => actualProgress < level.xp);
+
+  const xpToNextLevel = nextLevel ? nextLevel.xp - actualProgress : 0;
+
+  const progress = Math.min(actualProgress * 0.1, 100);
+
   return (
     <Card>
-      <SectionTitle>Progress Bar - Total XP</SectionTitle>
+      <SectionTitle>Your XP Progress and Current Level </SectionTitle>
       <ProgressWrapper>
-        <ProgressBar progress={actualProgress * 0.1}>
-          <ProgressText>{actualProgress} XP</ProgressText>
-        </ProgressBar>
+        <ProgressBar $progress={progress}></ProgressBar>
       </ProgressWrapper>
+
+      <SectionTitle>
+        <ProgressText>{actualProgress} / 1000 XP</ProgressText>
+        {currentLevel && (
+          <CurrentLevel>Level: {currentLevel.level}</CurrentLevel>
+        )}
+
+        {nextLevel && (
+          <NextLevel>
+            {xpToNextLevel > 0
+              ? `You need ${xpToNextLevel} more XP to reach ${nextLevel.level}`
+              : `You have reached the highest level! Congratulations`}
+          </NextLevel>
+        )}
+      </SectionTitle>
     </Card>
   );
 };
 
 export default ProgressCard;
-
-// Styled components for ProgressCard
 const Card = styled.section`
   width: 100%;
   background-color: ${({ theme }) => theme.colors.secondary.main};
@@ -47,16 +72,17 @@ const ProgressWrapper = styled.div`
   border-radius: ${({ theme }) => theme.borderRadius.medium};
   overflow: hidden;
   box-shadow: inset 0 2px 4px rgba(0, 0, 0, 0.2);
+  margin-bottom: ${({ theme }) => theme.spacing.md};
 `;
 
-const ProgressBar = styled.div<{ progress: number }>`
+const ProgressBar = styled.div<{ $progress: number }>`
   height: 40px;
   background: linear-gradient(
     90deg,
     ${({ theme }) => theme.colors.primary.main},
     ${({ theme }) => theme.colors.primary.dark}
   );
-  width: ${(props) => props.progress}%;
+  width: ${(props) => props.$progress}%;
   transition: width ${({ theme }) => theme.transitions.fast};
   display: flex;
   align-items: center;
@@ -67,4 +93,18 @@ const ProgressText = styled.span`
   color: ${({ theme }) => theme.colors.text.light};
   font-weight: ${({ theme }) => theme.typography.fontWeight.bold};
   font-size: ${({ theme }) => theme.typography.fontSize.md};
+`;
+
+const CurrentLevel = styled.p`
+  padding: ${({ theme }) => theme.spacing.md};
+  font-size: ${({ theme }) => theme.typography.fontSize.md};
+  font-weight: ${({ theme }) => theme.typography.fontWeight.bold};
+`;
+
+const NextLevel = styled.p`
+  font-size: ${({ theme }) => theme.typography.fontSize.sm};
+  color: white;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
 `;
